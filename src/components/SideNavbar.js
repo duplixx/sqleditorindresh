ff5f4893a347b8c49bc6aee51cba8cd0ac04d432
Indresh Goswami <indreshgoswami149@gmail.com>
2023-07-15 17:30:50 +0530

    inital commit

---
+++ src/components/SideNavbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./SideNavbar.css";
+import { exit, logo } from "../assets/svgFile";
+
+const links = [
+    { label: "Getting Started", value: "getting-started" },
+    { label: "Tables", value: "tables" },
+    { label: "Query Editor", value: "query-editor" }
+];
+
+const SideNavbar = () => {
+    return (
+        <div className="side-navbar">
+            <div className="app-logo">{logo()}</div>
+            <hr className="h-line" />
+            <div className="nav-links">
+                <ul className="link-items">
+                    {links.map((link) => (
+                        <li
+                            key={link.value}
+                            className={`link-item ${"query-editor" === link.value && "active"
+                                }`}
+                        >
+                            {link.label}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="logout">
+                <span className="icon">{exit()}</span>
+                <span className="text">Logout</span>
+            </div>
+        </div>
+    );
+};
+
+export default SideNavbar;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+    return <div className="coming-soon">COMING SOON</div>;
+};
+
+export default LandingPage;
